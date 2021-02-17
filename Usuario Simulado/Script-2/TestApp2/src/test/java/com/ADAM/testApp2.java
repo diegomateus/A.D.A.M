2d50270cb1873a51f8765737923298e6c786e58e
William Baquero <williambaqueror@gmail.com>
2021-02-17 08:46:18 -0500

    rutas de ejecucion random

--- Usuario Simulado/Script-2/TestApp2/src/test/java/com/ADAM/testApp2.java
+++ Usuario Simulado/Script-2/TestApp2/src/test/java/com/ADAM/testApp2.java
@@ -23,18 +23,10 @@ public class testApp2 {
 	
 	@Test
 	public void test() throws InterruptedException {
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[3]/a")).click();
-		Thread.sleep(2000);
 		
-		getOwner("Baqueroo");
+		
 		Thread.sleep(2000);
-		/*if(driver.findElement(By.xpath("/html/body/div/div/h2")).isEnabled()) {
-			driver.findElement(By.linkText("William Baquero")).click();
-		}else{*/
-			addOwner("William","Baquero","U.Javeriana","Bogota","3248034821");
-		//}
-		//editOwner("William","Baquero","U.Javeriana","Bogota","3248034821");
+		/*addOwner("William","Baquero","U.Javeriana","Bogota","3248034821");
 		addPet("buck","1998-12-12",1);
 		addPet("cuak","1998-12-12",2);
 		editOwner("","",""," Colina","");
@@ -42,9 +34,98 @@ public class testApp2 {
 		addPet("bruno","1998-12-12",3);
 		addVisit("Mordida");
 		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[4]/a")).click();
-		Thread.sleep(2000);
+		Thread.sleep(2000);*/
+		
+		for(int i=0; i<10 ;i++) {
+			principalAddress();
+		}
 		
 		driver.close();
+	}
+	
+	public void principalAddress() throws InterruptedException {
+		int route = (int) (Math.random()*5);
+		switch(route) {
+			case 0:
+				goHome();
+				Thread.sleep(2000);
+				break;
+			case 1:
+				goFind();
+				String title = driver.findElement(By.xpath("/html/body/div/div/h2")).getText();
+				while(title.compareTo("Find Owners")==0) {
+					Thread.sleep(2000);
+					desitionFind();
+					title = driver.findElement(By.xpath("/html/body/div/div/h2")).getText();
+				}
+				//String title2 = driver.findElement(By.xpath("/html/body/div/div/h2")).getText();
+				if(title.compareTo("Owners")==0) {
+					Thread.sleep(2000);
+					driver.findElement(By.xpath("/html/body/div/div/table/tbody/tr[1]/td[1]/a")).click();
+				}
+				Thread.sleep(2000);
+				ownerRoute();
+				break;
+			case 2:
+				goVeterian();
+				Thread.sleep(2000);
+				break;
+			case 3:
+				goError();
+				Thread.sleep(2000);
+				break;
+			default:
+				
+		}
+	}
+	public void ownerRoute() throws InterruptedException {
+		int route = (int) (Math.random()*5);
+		switch(route) {
+			case 0:
+				addVisit("vacunacion");
+				break;
+			case 1:
+				addPet("bruno","1998-12-12",3);
+				break;
+			case 2:
+				editOwner("","",""," Colina","");
+				break;
+			case 3:
+				editPet("","",3);
+				break;
+			default:
+				
+		}
+	}
+	
+	public void desitionFind() throws InterruptedException {
+		int decicion = (int) (Math.random()*2);
+		if(decicion==0){
+			addOwner("william","baquero","bogota","bogota","3219420");
+		}else{
+			findOwner("Baquero");
+		}
+	}
+	
+	public void goHome() {
+		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[2]/a")).click();
+	}
+	public void goFind() throws InterruptedException {
+		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[3]/a")).click();
+		int aux = (int) (Math.random()*2);
+		switch(aux) {
+		case 0:
+			addOwner("William","Baquero","U.Javeriana","Bogota","3248034821");
+			break;
+		case 1:
+			
+		}
+	}
+	public void goVeterian() {
+		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[4]/a")).click();
+	}
+	public void goError() {
+		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul/li[5]/a")).click();
 	}
 	
 	public void addOwner(String firstName, String lastName, String address, String city, String telephone) throws InterruptedException {
@@ -97,26 +178,26 @@ public class testApp2 {
 		
 	}
 	
-public void editPet(String name, String date, int i) throws InterruptedException {
+	public void editPet(String name, String date, int i) throws InterruptedException {
 	
-	driver.findElement(By.linkText("Edit Pet")).click();
-	Thread.sleep(1000);
-	driver.findElement(By.id("name")).sendKeys(name);
-	driver.findElement(By.id("birthDate")).sendKeys(date);
-	Select tipo = new Select(driver.findElement(By.id("type")));
-	tipo.selectByIndex(i);
-	Thread.sleep(1000);
-	driver.findElement(By.xpath("/html/body/div/div/form/div[2]/div/button")).click();
-	Thread.sleep(2000);
+		driver.findElement(By.linkText("Edit Pet")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.id("name")).sendKeys(name);
+		driver.findElement(By.id("birthDate")).sendKeys(date);
+		Select tipo = new Select(driver.findElement(By.id("type")));
+		tipo.selectByIndex(i);
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("/html/body/div/div/form/div[2]/div/button")).click();
+		Thread.sleep(2000);
 	
 	}
 	
-	public void getOwner(String lastName) throws InterruptedException {
+	public void findOwner(String lastName) throws InterruptedException {
 		driver.findElement(By.id("lastName")).sendKeys(lastName);
 		driver.findElement(By.xpath("/html/body/div/div/form/div[2]/div/button")).click();
 		Thread.sleep(1000);
 	}
-
 	
+	// /html/body/div/div/h2    /html/body/div/div/h2
 
 }
