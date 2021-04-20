6af953cb89ce4f4ed39d2a5a81a2e894568b62a3
William Baquero <williambaqueror@gmail.com>
2021-04-20 16:39:09 -0500

    merge grafos y modificaciones de lectura excel

---
+++ Analisis Dinamico/version Anterior/src/main/java/com/github/shehanperera/allmethods/AllMethod.java
@@ -0,0 +1,116 @@
+package com.github.shehanperera.allmethods;
+
+import net.bytebuddy.asm.Advice;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import javax.imageio.ImageIO;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.ext.JGraphXAdapter;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import com.mxgraph.layout.mxCircleLayout;
+import com.mxgraph.layout.mxIGraphLayout;
+import com.mxgraph.util.mxCellRenderer;
+
+public class AllMethod {
+
+	public static Stack<String> trace = new Stack<>();
+	public static Conexion graph = new Conexion();
+	public static Boolean bandera = false;
+	public static String nodoAnterior = null;
+	public static LocalDateTime time = LocalDateTime.now();
+	public static Boolean excelStatic = true;
+	public static int enter = 0;
+	
+	@Advice.OnMethodEnter
+    static void enterMethods(@Advice.Origin String method, @Advice.AllArguments Object[] ary) {
+		
+                System.out.println("Argument: "+Agent.paquete);
+		
+		try {
+			if(excelStatic){
+				System.out.println("Ingrese la ruta del grafo del analisis estatico");
+				Scanner enter = new Scanner(System.in); 
+				String fileRute = enter.nextLine ();
+				File file = new File (fileRute);
+				try{
+					graph.openBook(file);
+					excelStatic = false;
+				}
+				catch(Exception e){
+					//TODO: POR SI NO EXISTE EL DIRECTORIO
+				}
+			}
+		
+		String firma ="";
+		StringTokenizer st1 = new StringTokenizer(method, " ");
+        while (st1.hasMoreTokens()) {
+     	   String aux = st1.nextToken();
+            if(aux.contains("org.springframework")) {
+         	   StringTokenizer st2 = new StringTokenizer(aux, "(");
+         	   firma =  st2.nextToken();
+            }
+        }
+		trace.add(firma);
+		System.out.println(trace.size()+"---enter   "+firma);
+		if(trace.size()>2) {
+			bandera = true;
+			enter = trace.size();
+		}
+		}catch(Exception e) {
+			//trace.clear();
+		}
+    }
+
+    @Advice.OnMethodExit
+    static void getAllMethods() {
+    	try {
+    		String auxString = trace.pop();
+	    	if(bandera) {
+				if (!graph.verificarNodo(auxString)) {
+					graph.addNodo(auxString);
+				}
+				if(enter-trace.size()>1) {
+					graph.addArista(auxString, nodoAnterior, "", "");
+				}
+	    	}
+	    	nodoAnterior = auxString;
+
+	    	if(trace.empty()&&bandera) {
+	    		System.out.println("nuevo grafo");
+	    		/*if(java.time.Duration.between(LocalDateTime.now(),time).toMinutes()>2) {
+		    		DirectedGraph<String, DefaultEdge> g = new DefaultDirectedGraph<>(DefaultEdge.class);
+		        	
+		        	for(Entry<String, Conexion> entry : graph.entrySet()) {
+		        		g.addVertex(entry.getKey());
+		        		
+		        	}
+		        	for(Entry<String, Conexion> entry : graph.entrySet()) {
+		        		String auxNode = entry.getKey();
+		        		for (String temp : entry.getValue().getNodosConectados()) {
+		        		    g.addEdge(auxNode, temp);
+		        		}
+		        	}
+		        	JGraphXAdapter<String, DefaultEdge> graphAdapter = new JGraphXAdapter<>(g);
+		        	mxIGraphLayout layout = new mxCircleLayout(graphAdapter);
+		            layout.execute(graphAdapter.getDefaultParent());
+		            
+		            BufferedImage image = mxCellRenderer.createBufferedImage(graphAdapter, null, 2, Color.WHITE, true, null);
+		            File imgFile = new File("./temp/graph.png");
+		            ImageIO.write(image, "PNG", imgFile);
+	    		}*/
+	            bandera = false;
+	    	}
+	    }catch(Exception e) {
+			//id = 1;
+		}
+    }
+
+}
---
+++ Analisis Dinamico/version Anterior/src/main/java/com/github/shehanperera/allmethods/Conexion.java
@@ -0,0 +1,110 @@
+package com.github.shehanperera.allmethods;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class Conexion {
+	
+	//private HashSet<InfoArista> nodosConectados;
+	private HashMap<String, HashMap<String, InfoArista>> graph;
+	
+	public Conexion() {
+		super();
+		this.graph = new HashMap<>();
+	}
+	
+	 public void openBook(File file){
+	        try {
+	            FileInputStream fileInputStream = new FileInputStream(file);
+	            XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+	            openSheet(workbook,0);
+	            openSheet(workbook,1);
+	        }catch (Exception e){
+	            e.printStackTrace();
+	        }
+	    }
+
+	    private void openSheet(XSSFWorkbook workbook, int sheet){
+	        List<List<XSSFCell>> cellData = new ArrayList<>();
+
+	        try{
+	            XSSFSheet hssfSheet = workbook.getSheetAt(sheet);
+
+	            Iterator<Row> rowIterator = hssfSheet.rowIterator();
+
+	            while(rowIterator.hasNext()){
+	                XSSFRow hssfRow = (XSSFRow) rowIterator.next();
+	                Iterator<Cell> iterator = hssfRow.cellIterator();
+	                List<XSSFCell> cellTemp = new ArrayList<>();
+
+	                while (iterator.hasNext()){
+	                    XSSFCell hssfCell = (XSSFCell) iterator.next();
+	                    cellTemp.add(hssfCell);
+	                }
+	                cellData.add(cellTemp);
+	            }
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        getData(cellData,sheet);
+	    }
+
+	    private void getData(List<List<XSSFCell>> cellDataList, int num){
+
+	        switch (num){
+	            case 0:
+	                for (int i = 1; i < cellDataList.size(); i++){
+	                    List<?> cellTempList = cellDataList.get(i);
+	                    String id = cellTempList.get(0).toString();
+	                    String label = cellTempList.get(1).toString();
+	                    String type = cellTempList.get(2).toString();
+	                    String subtype = "null";
+	                    if(cellTempList.size() > 2){
+	                        subtype = cellTempList.get(3).toString();
+	                    }
+	                    this.addNodo(label);
+	                }
+	                break;
+	            case 1:
+	                for (int i = 1; i < cellDataList.size(); i++){
+	                    List<?> cellTempList = cellDataList.get(i);
+	                    String source = cellTempList.get(0).toString();
+	                    String target = cellTempList.get(1).toString();
+	                    String label = cellTempList.get(2).toString();
+	                    String type = cellTempList.get(3).toString();
+	                    this.addArista(source, target, type, label);
+	                
+	                }
+	                break;
+	            default:
+	                // TODO: agregar error
+	        }
+	    }
+	    
+	    public void addNodo(String nombreNodo) {
+			this.graph.put(nombreNodo, new HashMap<>());
+		}
+		
+		public boolean verificarNodo(String nombreNodo) {
+			return this.graph.containsKey(nombreNodo);
+		}
+		
+		public void addArista(String keyNodo, String nodoDestino, String tipo, String label) {
+			this.graph.get(keyNodo).put(nodoDestino, new InfoArista(tipo, label));
+		}
+
+}
